8c396af4e469b2355f1030017b67e5ad16eb3cf3
Sergei Shliakhin <sshlyahin@yahoo.com>
2021-07-29 01:49:54 +0300

    init: Right on target - project

--- Right on target/ViewController.swift
+++ Right on target/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    @IBOutlet var slider: UISlider!
+    
+    var number = 0
+    var points = 0
+    var round = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func checkNumber() {
+        if round == 0 {
+            number = Int.random(in: 1...50)
+            label.text = String(number)
+            round = 1
+        } else {
+            let numSlider = Int(slider.value.rounded())
+            if numSlider > number {
+                points += 50 - numSlider + number
+            } else if numSlider < number {
+                points += 50 - number + numSlider
+            } else {
+                points += 50
+            }
+            if round == 5 {
+                let alert = UIAlertController(title: "Game over", message: "You got \(points) points", preferredStyle: .alert)
+                let newGameAction = UIAlertAction(title: "New game", style: .default, handler: nil)
+                alert.addAction(newGameAction)
+                present(alert, animated: true, completion: nil)
+                round = 1
+                points = 0
+            } else {
+                round += 1
+            }
+            number = Int.random(in: 1...50)
+            label.text = String(describing: number)
+        }
+    }
 }
 
